f96c00ed72f8e67a3a723959e8b292f631434110
도비 <jm.dev.study@gmail.com>
2023-05-12 20:50:25 +0900

    ✨ 데이터베이스의 properties 값에서 필요한 데이터만 return 하는 로직 추가 - 관련된 타입 추가

--- src/lib/notion.ts
+++ src/lib/notion.ts
@@ -1,10 +1,11 @@
+import { DataBaseItemType, PostType } from '@/types/database';
 import { Client } from '@notionhq/client';
 
 const notion = new Client({
   auth: process.env.NEXT_PUBLIC_API_KEY,
 });
 
-export const getNotionPosts = async () => {
+export const getNotionPosts = async (): Promise<PostType[]> => {
   const database = await notion.databases.query({
     database_id: `${process.env.NEXT_PUBLIC_DATABASE_ID}`,
     filter: {
@@ -16,5 +17,25 @@ export const getNotionPosts = async () => {
     sorts: [{ timestamp: 'created_time', direction: 'descending' }],
   });
 
-  return database.results;
+  const dataBaseItems: DataBaseItemType[] = [];
+  database.results.forEach(post => {
+    return (
+      'properties' in post &&
+      dataBaseItems.push({ ...post.properties, id: post.id } as DataBaseItemType)
+    );
+  });
+
+  const posts: PostType[] = dataBaseItems.map(post => {
+    return {
+      id: post.id,
+      title: post.title.title[0]?.plain_text || '제목을 입력해주세요',
+      slug: post.slug.rich_text[0]?.plain_text || '경로를 입력해주세요',
+      tags: post.tags.multi_select,
+      createTime: post.created_time.created_time,
+      description: post.description.rich_text[0]?.plain_text || '',
+      thumbnail: post.thumbnail.files[0],
+    };
+  });
+
+  return posts;
 };
---
+++ src/types/database.ts
@@ -0,0 +1,35 @@
+import {
+  PageObjectResponse,
+  SelectPropertyItemObjectResponse,
+} from '@notionhq/client/build/src/api-endpoints';
+
+type PropertyValue = Pick<PageObjectResponse, 'properties'>['properties'][string];
+type ExtractPropertyValue<T extends PropertyValue['type']> = Extract<PropertyValue, { type: T }>;
+
+type TitlePropertyType = ExtractPropertyValue<'title'>;
+type RichTextPropertyType = ExtractPropertyValue<'rich_text'>;
+type MultiSelectPropertyType = ExtractPropertyValue<'multi_select'>;
+type CheckboxPropertyType = ExtractPropertyValue<'checkbox'>;
+type FilesPropertyType = ExtractPropertyValue<'files'>;
+type EditedTimePropertyType = ExtractPropertyValue<'created_time'>;
+
+export interface DataBaseItemType {
+  id: string;
+  title: TitlePropertyType;
+  description: RichTextPropertyType;
+  slug: RichTextPropertyType;
+  tags: MultiSelectPropertyType;
+  active: CheckboxPropertyType;
+  created_time: EditedTimePropertyType;
+  thumbnail: FilesPropertyType;
+}
+
+export interface PostType {
+  id: string;
+  title: string;
+  slug: string;
+  tags: SelectPropertyItemObjectResponse['select'][];
+  createTime: string;
+  description: string;
+  thumbnail: any;
+}
