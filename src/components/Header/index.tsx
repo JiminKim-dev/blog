eb30e44d148c8fd08cfc24dc5f74cd13175aa54e
도비 <jm.dev.study@gmail.com>
2023-05-13 00:18:37 +0900

    💄 Header 컴포넌트 스타일 수정 - 그라이언트 색 적용, hover시 글자색 변경

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,15 +2,23 @@ import Link from 'next/link';
 
 const Header = () => {
   return (
-    <header className="flex items-center justify-between p-4">
-      <Link href="/" className="text-3xl font-bold">
-        Dobby's Blog
-      </Link>
-      <nav className="flex gap-4">
-        <Link href="/">Home</Link>
-        <Link href="/about">about</Link>
-        <Link href="/posts">Posts</Link>
-      </nav>
+    <header className="px-4 py-6 bg-gradient-to-r from-cyan-500 to-blue-500">
+      <div className="flex items-center justify-between max-w-screen-lg m-auto">
+        <Link href="/" className="text-2xl font-bold text-white hover:text-stone-800">
+          Dobby's Blog
+        </Link>
+        <nav className="flex gap-4 text-white">
+          <Link href="/" className="hover:text-stone-800">
+            Home
+          </Link>
+          <Link href="/about" className="hover:text-stone-800">
+            About
+          </Link>
+          <Link href="/posts" className="hover:text-stone-800">
+            Posts
+          </Link>
+        </nav>
+      </div>
     </header>
   );
 };
