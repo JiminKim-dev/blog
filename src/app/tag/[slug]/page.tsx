258f816d38c3ab8cce215b392b704f576ab98b1e
도비 <jm.dev.study@gmail.com>
2023-06-06 20:21:21 +0900

    ✨ tag 페이지 렌더링을 위한 page 컴포넌트 생성 - 해당 태그와 관련된 포스트를 렌더링한다.

---
+++ src/app/tag/[slug]/page.tsx
@@ -0,0 +1,24 @@
+import PostContainer from '@/components/Posts/PostContainer';
+import { getFilterdPosts } from '@/lib/notion';
+
+type Props = {
+  params: {
+    slug: string;
+  };
+};
+
+const TagPage = async ({ params: { slug } }: Props) => {
+  const filterdTagPosts = await getFilterdPosts(slug);
+
+  return (
+    <div className="w-full">
+      <h1 className="mt-8 text-5xl font-extrabold text-stone-800">Tag</h1>
+      <p className="py-6">{slug} 태그와 관련된 포스트 입니다.</p>
+      <section className="flex flex-col gap-4">
+        <PostContainer posts={filterdTagPosts} />
+      </section>
+    </div>
+  );
+};
+
+export default TagPage;
