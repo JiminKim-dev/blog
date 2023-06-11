8107bf483858b853f0c2e3552db7589ee09f7c47
도비 <jm.dev.study@gmail.com>
2023-06-12 00:10:46 +0900

    🔧 외부 이미지 로드를 위한 next.config 옵션 추가

--- next.config.js
+++ next.config.js
@@ -3,6 +3,15 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-}
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 's3.us-west-2.amazonaws.com',
+        pathname: '/secure.notion-static.com/**',
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
