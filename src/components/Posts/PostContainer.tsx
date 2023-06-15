7ec470bba46334b3522f4b174efb8cbdf5dc80f4
도비 <jm.dev.study@gmail.com>
2023-06-15 23:42:49 +0900

    💄 PostContainer의 list 관련 기본 스타일 제거

--- src/components/Posts/PostContainer.tsx
+++ src/components/Posts/PostContainer.tsx
@@ -3,7 +3,7 @@ import PostCard from '@/components/Posts/PostCard';
 
 const PostContainer = ({ posts }: { posts: PostType[] }) => {
   return (
-    <ul className="flex flex-col w-full gap-4">
+    <ul className="flex flex-col w-full gap-4 p-0 list-none">
       {posts.map(post => (
         <li key={post.id}>
           <PostCard key={post.id} post={post} />
