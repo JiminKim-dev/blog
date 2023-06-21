8d4187b8eb89822e2533a228887d788d0820d64a
도비 <jm.dev.study@gmail.com>
2023-06-21 22:23:39 +0900

    ✨ tags 페이지 생성 및 일부 UI 구현

---
+++ src/app/tags/page.tsx
@@ -0,0 +1,10 @@
+const TagPage = async () => {
+  return (
+    <div className="w-full">
+      <h1 className="my-8 text-5xl font-extrabold text-stone-800">Tags</h1>
+      <section className="flex flex-col gap-6"></section>
+    </div>
+  );
+};
+
+export default TagPage;
