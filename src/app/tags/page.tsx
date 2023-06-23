bdfe2fde631bb50d7130fb181630bac08fb0453c
도비 <jm.dev.study@gmail.com>
2023-06-23 16:33:45 +0900

    ✨ Tags 페이지에 AllTags 적용 및 컴포넌트 이름 변경

--- src/app/tags/page.tsx
+++ src/app/tags/page.tsx
@@ -1,10 +1,17 @@
-const TagPage = async () => {
+import AllTags from '@/components/AllTags';
+import { getNotionPosts } from '@/lib/notion';
+
+const TagsPage = async () => {
+  const posts = await getNotionPosts();
+
   return (
     <div className="w-full">
       <h1 className="my-8 text-5xl font-extrabold text-stone-800">Tags</h1>
-      <section className="flex flex-col gap-6"></section>
+      <section className="flex flex-col gap-6">
+        <AllTags posts={posts} />
+      </section>
     </div>
   );
 };
 
-export default TagPage;
+export default TagsPage;
