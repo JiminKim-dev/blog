fee977430e02b83999742398284c4065957cead7
도비 <jm.dev.study@gmail.com>
2023-06-23 16:34:41 +0900

    🩹 tags 하위 페이지 컴포넌트의 이름을 TagDetailPage으로 변경

--- src/app/tags/[slug]/page.tsx
+++ src/app/tags/[slug]/page.tsx
@@ -7,12 +7,12 @@ type Props = {
   };
 };
 
-const TagPage = async ({ params: { slug } }: Props) => {
+const TagDetailPage = async ({ params: { slug } }: Props) => {
   const filterdTagPosts = await getFilterdPosts(slug);
 
   return (
     <div className="w-full">
-      <h1 className="mt-8 text-5xl font-extrabold text-stone-800">Tag</h1>
+      <h1 className="mt-8 text-5xl font-extrabold text-stone-800">{slug}</h1>
       <p className="py-6">{slug} 태그와 관련된 포스트 입니다.</p>
       <section className="flex flex-col gap-4">
         <PostContainer posts={filterdTagPosts} />
@@ -21,4 +21,4 @@ const TagPage = async ({ params: { slug } }: Props) => {
   );
 };
 
-export default TagPage;
+export default TagDetailPage;
